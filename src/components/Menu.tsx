db28bc695774701b874a890c03ff5f75f6da2c5d
Florentino Naarro Diaz <tino.navarro@hotmail.com>
2023-08-22 11:46:25 -0600

    menu fixed

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -9,13 +9,14 @@ type Props = {
 
 const Menu = ({ active}: Props) => {
   const click = (e: any) => {
-
+    const toggle = document.querySelector('.toggle-menu') as HTMLElement;
+    toggle.click();
   }
   return (
     <ul className={`main-menu ${active ? 'is-active' : ''}`}>
       {
         menuItems.map((item: any, i:number) => (
-          <li key={i} className="main-menu__item">
+          <li key={i} className="main-menu__item" >
             <a className="main-menu__link data-link" onClick={click} data-link href={`#${item.title}`}>{item.title}</a>
           </li>
         ))
